bf1bb792f1f639582f92434f94100492e816bc29
Sam Brown <sjbb5@icloud.com>
2025-06-04 11:04:18 -0600

    June 4

---
+++ notebooks/SURF/.ipynb_checkpoints/stations-checkpoint.py
@@ -0,0 +1,19 @@
+# sam brown
+# sam_brown@mines.edu
+# 06/03/2025
+# Class for stations to better organize code
+
+
+class Station:
+    
+    def __init__(self, name,):
+        self.name = name
+        
+    
+    # get averages
+
+    # plot station
+
+    # get coordinates
+
+    # get lat, long
---
+++ notebooks/SURF/stations.py
@@ -0,0 +1,22 @@
+# sam brown
+# sam_brown@mines.edu
+# 06/03/2025
+# Class for stations to better organize code and possibly allow for more effective standardization
+
+
+class Station:
+    
+    def __init__(self, name,):
+        self.name = name
+        self.chars = name[0:2]
+        self.num = name[2:4]
+    
+    # get averages
+
+    # plot station
+
+    # get coordinates
+
+    # get lat, long
+
+    # get tide data
