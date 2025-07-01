9af21bd5d4fe859ec8c53f035493dac911dc0008
Sam Brown <sjbb5@icloud.com>
2025-07-01 07:11:55 -0600

    Jul 1

---
+++ notebooks/SURF/Datastream.py
@@ -0,0 +1,183 @@
+import os
+import pandas as pd
+import numpy as np
+import datetime
+from typing import Tuple
+
+from util.coordinate_transforms import ll2xy
+class Datastream:
+    """Data for one station"""
+
+    def __init__(
+        self, sta: os.DirEntry, name: str, years: list) -> None:
+        """Initialize a data stream.
+
+        Parameters
+        ----------
+        sta: os.DirEntry
+            Path to station
+        name : str
+            Name of the station (Typically the directory name)
+        years : list
+            List of year folders to include in this data stream.
+        """
+        self.sta = sta
+        self.name = name
+        self.years = years
+
+        data = self.make_data_stream(sta, years)
+        self.data = data
+
+    def make_data_stream(self, sta: os.DirEntry, years: list) -> pd.DataFrame:
+        """
+        Return a Pandas data frame for all .pos files in years.
+
+        Takes input directory sta with year subdirectories containing .pos files.
+        Starting from the first file in the first year, append days until all files
+        in the directory have been added.
+
+        Parameters
+        ----------
+        dir: os.DirEntry
+            Input directory tree with structure described in top comment [string]
+        years: list of strings
+            Years to be run
+
+        Returns
+        data: pd.DataFrame
+            Multi-day data frame
+        """
+
+        data = pd.DataFrame()
+        for year in years:
+            for folder in os.scandir(os.fspath(self.sta)):
+                # Folder is an os.DirEntry object
+                if folder.is_dir():
+                    if folder.name == year:
+                        for gps in sorted(os.listdir(folder.path)):
+                            # Ignore xyzt, zip files
+                            if gps.endswith(".pos") and not gps.startswith("."):
+                                ind_data, flip = self.load(f"{folder.path}/{gps}")
+                                # Reorder the data if using CSRS-PPP 2024 pre 2018
+                                if flip:
+                                    ind_data = ind_data.reindex(
+                                        index=ind_data.index[::-1]
+                                    )
+                                data = pd.concat([data, ind_data], ignore_index=True)
+        return data
+
+    def load(self, file: str) -> Tuple[pd.DataFrame, bool]:
+        """
+        Load processed gps data file into pandas table.
+
+        Assumes data at 00,15,30,45 second intervals and removes all other data.
+        For slw1, 46 second data are moved to 45 seconds.
+
+
+        Parameters
+        file - .pos precise point solution file, Natural Resources Canada [string]
+
+        Returns
+        data - Pandas DataFrame with the following columns, extracted from file
+            longitude - Geodetic longitude
+            latitude - Geodetic latitude
+            elevation - Elevation
+            time - Time as datetime object
+            day_of_year - Time as julian day
+            x - Antarctic Polar Stereographic x
+            y - Antarctic Polar Stereographic y
+            dist - Euclidian distance from starting position in meters
+        flip - flag to flip order of readings for 2024 csrs-ppp data
+        """
+
+        data = pd.DataFrame()  # Create Pandas DataFrame
+        flip = False
+        # Read data file
+
+        # Convert longitude and latitude from deg min sec to fractional degrees
+        # Three different possible file formats so try one first and try the other if it
+        # throws a not found exception.
+        try:
+            # CSRS-PPP 2024
+            d = pd.read_csv(
+                file, skiprows=3, sep="\\s+"
+            )  # delim_whitespace=True depreciated, used \\s+ instead
+            # Only take 15 or 30 sec intervals if data is finer spaced
+            d = d.loc[
+                d["HR:MN:SS.SS"].str.endswith("00.00")
+                | d["HR:MN:SS.SS"].str.endswith("15.00")
+                | d["HR:MN:SS.SS"].str.endswith("30.00")
+                | d["HR:MN:SS.SS"].str.endswith("45.00")
+                | d["HR:MN:SS.SS"].str.endswith("16.00")  # Slw1 only, move to 15 sec
+                | d["HR:MN:SS.SS"].str.endswith("46.00")  # Slw1 only, move to 45 sec
+            ]
+            data["longitude"] = d["LONDD"] - d["LONMN"] / 60 - d["LONSS"] / 60 / 60
+            data["latitude"] = d["LATDD"] - d["LATMN"] / 60 - d["LATSS"] / 60 / 60
+            data["time"] = pd.to_datetime(d["YEAR-MM-DD"] + "T" + d["HR:MN:SS.SS"])
+            data.loc[data["time"].dt.second == 46, "time"] = data[
+                "time"
+            ] - pd.Timedelta(seconds=1)  # Slw1 only, move to 45 sec
+            data.loc[data["time"].dt.second == 16, "time"] = data[
+                "time"
+            ] - pd.Timedelta(seconds=1)  # Slw1 only, move to 15 sec
+            data["day_of_year"] = d["DAYofYEAR"]
+
+        # If not in 2024 format, may be in an older format, tested for below...
+        except Exception as e:
+            try:
+                d = pd.read_csv(file, skiprows=7, sep="\\s+")
+                data["longitude"] = (
+                    d["LON(d)"] - d["LON(m)"] / 60 - d["LON(s)"] / 60 / 60
+                )
+                data["latitude"] = (
+                    d["LAT(d)"] - d["LAT(m)"] / 60 - d["LAT(s)"] / 60 / 60
+                )
+                data["time"] = pd.to_datetime(d["YEAR-MM-DD"] + "T" + d["HR:MN:SS.SSS"])
+                data["day_of_year"] = d["DOY"]
+            except Exception as e:
+                try:
+                    d = pd.read_csv(file, skiprows=5, sep="\\s+")
+                    data["longitude"] = (
+                        d["LONDD"] - d["LONMN"] / 60 - d["LONSS"] / 60 / 60
+                    )
+                    data["latitude"] = (
+                        d["LATDD"] - d["LATMN"] / 60 - d["LATSS"] / 60 / 60
+                    )
+                    data["time"] = pd.to_datetime(
+                        d["YEAR-MM-DD"] + "T" + d["HR:MN:SS.SS"]
+                    )
+                    data["day_of_year"] = d["DAYofYEAR"]
+                except Exception as e:
+                    d = pd.read_csv(file, skiprows=6, sep="\\s+")
+                    data["longitude"] = (
+                        d["LON(d)"] - d["LON(m)"] / 60 - d["LON(s)"] / 60 / 60
+                    )
+                    data["latitude"] = (
+                        d["LAT(d)"] - d["LAT(m)"] / 60 - d["LAT(s)"] / 60 / 60
+                    )
+                    data["time"] = pd.to_datetime(
+                        d["YEAR-MM-DD"] + "T" + d["HR:MN:SS.SSS"]
+                    )
+                    data["day_of_year"] = d["DOY"]
+
+        data["elevation"] = d["HGT(m)"]
+        data["sats"] = d["NSV"]
+        data["GDOP"] = d["GDOP"]
+
+        # Convert to Antarctic Polar Stereographic
+        x, y = ll2xy(lon=data["longitude"], lat=data["latitude"])
+        data["x"] = x
+        data["y"] = y
+
+        # Look at data and decide if to flip
+        if len(data.index) > 1:
+            diff = data["time"].iloc[0] - data["time"].iloc[1]
+            if diff > datetime.timedelta(seconds=0):
+                flip = True
+
+            x0 = data["x"].iloc[0]
+            y0 = data["y"].iloc[0]
+
+            data["dist"] = np.sqrt((data["x"] - x0) ** 2 + (data["y"] - y0) ** 2)
+
+        return data, flip
